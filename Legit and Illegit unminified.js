716573b62911a73ad37aedcafdfd1c8352d42e72
David Metcalfe <DavidMetcalfe@users.noreply.github.com>
2018-04-30 17:50:33 -0700

    Create Legit and Illegit unminified.js

---
+++ Legit and Illegit unminified.js
@@ -0,0 +1,20 @@
+// [ === Toggleable autoclicker === ]
+// Activating will enable, and a second toggle will deactivate.
+
+// ** Legit version (doesn't activate Game.autoclickerDetected)
+if (autoClicker){
+	clearInterval(autoClicker);
+}
+else {
+    var autoClicker = "";
+	autoClicker = setInterval(Game.ClickCookie, 250);
+}
+
+// ** Illegit version (DOES activate Game.autoclickerDetected)
+if (autoClicker){
+	clearInterval(autoClicker);
+}
+else {
+    var autoClicker = "";
+	autoClicker = setInterval(Game.ClickCookie, 10);
+}
